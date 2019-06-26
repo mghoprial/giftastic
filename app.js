f7f2655c513b7c94d54f125d2e72006da930c416
Mark Ghoprial <markghoprial@Marks-MacBook-Pro.local>
2019-06-26 09:58:26 -0700

    last minute updates

--- app.js
+++ app.js
@@ -1,27 +1,53 @@
 $(document).ready(function () {
-    let userInput = $("#userInput").val();
+
     // create inital list of animals
     let animalList = ["cat", "liger", "chicken"];
+    renderButtons(animalList);
     // onClick submit run getGiphy function
     $("#submitAnimal").on('click', function () {
-        getGiphy(userInput);
+        console.log("submitAnimal eventhandler");
+        let userInput = $("#userInput").val();
+        // getGiphy(userInput);
+        let newAnimal = $("input").eq(0).val();
+
+        if (newAnimal.length > 2) {
+            animalList.push(newAnimal);
+        }
         renderButtons(animalList);
+        getGiphy(userInput);
+
+
     })
     // onClick animalBtn run getGiphy function
-    $(".animalBtn").on("click", function () {
-        getGiphy($(this).val());
-    })
+    $(document).on("click", ".animalBtn", function () {
+        let input = $(this).text();
+        getGiphy(input);
+
+        console.log("animalBtn eventhandler");
+        let state = $(this).attr("data-state");
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+        }
+        else {
+            $(this).attr("src", $(this).attr("data=still"));
+            $(this).attr("data-state", "still");
+        }
+    });
     // for each animal, create a button and set its text to the current animal
 
 
     function renderButtons(arrayOfAnimals) {
-        arrayOfAnimals.forEach(function (animal) {
+        console.log("renderButtons");
+        $("#animalButton").empty();
+        arrayOfAnimals.forEach(function (animal) { //creates a button for each animal listed/ created
             var animalButtons = $("<button>")
             animalButtons.text(animal);
             // add class to all animal buttons
             animalButtons.attr("class", "animalBtn");
             // get an element by ID name and append animalButtons on onto that element
             $("#animalButton").append(animalButtons);
+
         });
     }
 
@@ -29,16 +55,41 @@ $(document).ready(function () {
     // if the event.target.onclick === true run the getGiphy function on the current button
 
     function getGiphy(searchTerm) {
-        if (searchTerm) {
-            animalList.push(searchTerm)
-            let API_KEY = "zuvJBFpNQACuT3Cy6xLLx1FlvLh20lXJ";
-            let queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + API_KEY + "&q=" + searchTerm + "t&limit=25&offset=0&rating=G&lang=en";
-            $.ajax({ url: queryUrl, methed: "GET" }).then(function (response) {
-                console.log(response);
-            })
-        } else {
-            console.log("Error");
-        }
+        console.log("getGiphy");
+        console.log(searchTerm);
+
+        animalList.push(searchTerm)
+        let API_KEY = "zuvJBFpNQACuT3Cy6xLLx1FlvLh20lXJ";
+        let queryUrl = "http://api.giphy.com/v1/gifs/search?api_key=" + API_KEY + "&q=" + searchTerm + "t&limit=25&offset=0&rating=G&lang=en";
+        $.ajax({ url: queryUrl, methed: "GET" }).then(function (response) {
+            console.log(response);
+
+            let results = response.data;
+            console.log("results", results);
+
+            for (var i = 0; i < results.length; i++) {
+                let animalDiv = $("<div class= \"animal\">");
+
+                let rating = results[i].rating;
+
+                let p = $("<p>").text("Rating: " + rating);
+
+                let animated = results[i].images.fixed_height.url;
+                let still = results[i].images.fixed_height_still.url;
+
+                let animalImage = $("<img>");
+                animalImage.attr("src", still);
+                animalImage.attr("data-still", still);
+                animalImage.attr("data-animate", animated);
+                animalImage.attr("data-state", "still");
+                animalImage.addClass("animal-image");
+
+                animalDiv.append(p);
+                animalDiv.append(animalImage);
+                $("#images").append(animalDiv);
+            }
+        })
+
     }
 
     //render giphys - function that takes the response from the API and creates elements using jquerry
@@ -50,77 +101,23 @@ $(document).ready(function () {
 
 
 
-        //function that pulls images from giphy
+    //function that pulls images from giphy
 
-        $("#addAnimal").on('click', function () {
-            $("#images").empty();
-            $(".animalButton").removeClass("active");
-            $(this).addClass("active");
+    $("#addAnimal").on('click', function () {
+        console.log("#addAnimal eventhandler");
+        $("#images").empty();
+        $(".animalButton").removeClass("active");
+        $(this).addClass("active");
 
-            let type = $("#animal").val();
-            // let querryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + API_KEY + "+&q=" + type + "t&limit=25&offset=0&rating=G&lang=en";
+        let type = $("#animal").val();
+        // let querryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + API_KEY + "+&q=" + type + "t&limit=25&offset=0&rating=G&lang=en";
 
-            let querryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + API_KEY + "&q=" + type + "t&limit=25&offset=0&rating=G&lang=en";
-            // before API request
-            animalList.push(type);
-            console.log(animalList);
-            var animalButtons = $("<button>").text(animalList);
-            $("#animalButton").append(animalButtons);
-            // ============================
-            $.ajax({ url: querryUrl, method: "GET" }).then(function (response) {
-
-                let results = response.data;
-
-                for (var i = 0; i < results.length; i++) {
-                    let animalDiv = $("<div class= \"animal\">");
-
-                    let rating = results[i].rating;
-
-                    let p = $("<p>").text("Rating: " + rating);
-
-                    let animated = results[i].images.fixed_height.url;
-                    let still = results[i].images.fixed_height_still.url;
-
-                    let animalImage = $("<img>");
-                    animalImage.attr("src", still);
-                    animalImage.attr("data-still", still);
-                    animalImage.attr("data-animate", animated);
-                    animalImage.attr("data-state", "still");
-                    animalImage.addClass("animal-image");
-
-                    animalDiv.append(p);
-                    animalDiv.append(animalImage);
-                    $("#images").append(animalDiv);
-                }
-            }).catch(err => console.log(err));
-
-        });
-
-
-        $(document).on("click", "animal-image", function () {
-            let state = $(this).attr("data-state");
-
-            if (state === "still") {
-                $(this).attr("src", $(this).attr("data-animate"));
-            }
-            else {
-                $(this).attr("src", $(this).attr("data=still"));
-                $(this).attr("data-state", "still");
-            }
-        });
-
-        $("#add-animal").on("click", function (event) {
-            event.preventDefault();
-            let newAnimal = $("input").eq(0).val();
-
-            if (newAnimal.length > 2) {
-                animalList.push(newAnimal);
-            }
-
-
-
-        })
-
-
-
+        let querryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + API_KEY + "&q=" + type + "t&limit=25&offset=0&rating=G&lang=en";
+        // before API request
+        animalList.push(type);
+        console.log(animalList);
+        var animalButtons = $("<button>").text(animalList);
+        $("#animalButton").append(animalButtons);
+        // ============================
+    })
 })
